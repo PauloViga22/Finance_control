4795f88b9219cd9386fed715c7985d7f0c3c968c
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2020-01-29 16:12:38 -0800

    remove old stuffs

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,3 @@
-const Note = require('../models/note');
 const Transaction = require('../models/transaction');
 
 exports.getHomePage = (req, res, next) => {
@@ -58,52 +57,3 @@ exports.updateTransaction = (req, res, next) => {
   res.redirect('/transactions/' + transactionId);
 }
 
-
-
-exports.writeNote = (req, res, next) => {
-  res.render('write');
-}
-
-exports.postNote = (req, res, next) => {
-  const note = new Note(req.body.author, req.body.title, req.body.content);
-  note.saveNote();
-  res.redirect('/');
-}
-
-exports.readNotes = (req, res, next) => {
-  Note.getAll().then(notes => {
-    res.render('read', {
-      notes: notes
-    });
-  });
-}
-
-exports.seeNote = (req, res, next) => {
-  const noteId = req.params.noteId;
-  const isEditing = req.query.edit;
-
-  Note.getAll().then(notes => {
-    const note = notes.find((nt) => nt._id == noteId)
-
-    res.render('see', {
-      note: note,
-      editMode: isEditing
-    });
-  });
-}
-
-exports.deleteNote = (req, res, next) => {
-  const noteId = req.body.noteId;
-  console.log(noteId);
-  Note.deleteNote(noteId);
-  res.redirect('/read');
-}
-
-exports.updateNote = (req, res, next) => {
-  const noteId = req.body.noteId;
-  const title = req.body.title;
-  const content = req.body.content;
-
-  Note.updateNote(noteId, title, content);
-  res.redirect('/notes/' + noteId);
-}
--- routes/route.js
+++ routes/route.js
@@ -4,22 +4,14 @@ const router = express.Router();
 
 router.get('/', controller.getHomePage);
 
-router.get('/write', controller.writeNote);
-router.post('/write', controller.postNote);
-
 router.get('/writeTransaction', controller.writeTransaction);
 router.post('/writeTransaction', controller.postTransaction);
 
-router.get('/notes/:noteId', controller.seeNote);
 router.get('/transactions/:transactionId', controller.seeTransaction);
 
-router.post('/see', controller.deleteNote);
 router.post('/seeTransaction', controller.deleteTransaction);
 
-router.post('/update', controller.updateNote);
 router.post('/updateTransaction', controller.updateTransaction);
-
-router.get('/read', controller.readNotes);
 
 router.get('/readTransaction', controller.readTransactions);
 
